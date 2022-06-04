5544ddca73358e7346291d0f9f6a1a14822659b0
Mert Öztat <mertoztat@gmail.com>
2022-06-04 17:34:05 +0300

    random image feed added

--- 48-Random Image Feed/script.js
+++ 48-Random Image Feed/script.js
@@ -1,1 +1,27 @@
+const container = document.querySelector(".container");
+const unsplash_URL = "https://source.unsplash.com/random/";
+const reload = document.getElementById("reload");
+const rows = 10;
 
+reload.addEventListener("click", () => {
+  window.setTimeout(() => {
+    window.location.reload(true);
+  }, 80);
+});
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplash_URL}${getRandomSize()}`;
+
+  container.appendChild(img);
+}
+
+console.log(getRandomSize());
+
+function getRandomSize() {
+  return `${getRandomNumber()}*${getRandomNumber()}`;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 300;
+}
